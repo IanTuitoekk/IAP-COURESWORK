fbe1100d9b9bed3ca33b87ac2be109c6474dd70b
IanTuitoekk <iantuitoek4@gmail.com>
2024-10-05 15:45:44 +0300

    Implemented input validation

--- processes/auth.php
+++ processes/auth.php
@@ -16,6 +16,53 @@ class auth{
             }else{
                 die($insert);
             }
+            // Implement input validation and error handling
+// =============================================
+// Sanitize all inputs
+
+// verify that the fullname has only letters, space, dash, quotation
+if(ctype_alpha(str_replace(" ", "", str_replace("\'", "", $fullname))) === FALSE){
+    $errors['nameLetters_err'] = "Invalid name format: Full name must contain letters and spaces only etc " . $fullname;
+}
+
+// verify that the email has got the correct format
+if(!filter_var($email_address, FILTER_VALIDATE_EMAIL)){
+    $errors['email_format_err'] = 'Wrong email format';
+}
+
+// verify that the email domain is authorized (@strathmore.edu, @gmail.com, @yahoo.com, @mada.co.ke) and not (@yanky.net)
+$conf['valid_domains'] = ["strathmore.edu", "gmail.com", "yahoo.com", "mada.co.ke", "outlook.com", "STRATHMORE.EDU", "GMAIL.COM", "YAHOO.COM", "MADA.CO.KE", "OUTLOOK.COM"];
+
+$arr_email_address = explode("@", $email_address);
+$spot_dom = end($arr_email_address);
+$spot_user = reset($arr_email_address);
+
+if(!in_array($spot_dom, $conf['valid_domains'])){
+    $errors['mailDomain_err'] = "Invalid email address domain. Use only: " . implode(", ", $conf['valid_domains']);
+}
+$exist_count = 0;
+// Verify Email Already Exists
+$spot_email_address_res = $conn->count_results(sprintf("SELECT email FROM users WHERE email = '%s' LIMIT 1", $email_address));
+// die($spot_email_address_res);
+if ($spot_email_address_res > $exist_count){
+    $errors['mailExists_err'] = "Email Already Exists";
+}
+
+// Verify Username Already Exists
+$spot_username_res = $conn->count_results(sprintf("SELECT username FROM users WHERE username = '%s' LIMIT 1", $username));
+if ($spot_username_res > $exist_count){
+    $errors['usernameExists_err'] = "Username Already Exists";
+}
+
+// Verify if username contain letters only
+if (!ctype_alpha($username)) {
+    $errors['usernameLetters_err'] = "Invalid username format. Username must contain letters only";
+}
+
+// Verify that the password is identical to the repeat passsword
+// verify that the password length is between 4 and 8 characters
+if(!count($errors)){
+
         }
     }
 }
