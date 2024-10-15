20e4ff143db4b02ded9f219b62dfc987d39c6cb1
IanTuitoekk <iantuitoek4@gmail.com>
2024-10-15 21:06:06 +0300

    Updated

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
-<html>
-    <!-- <head>Index</head> -->
-    <body>
-        <!-- <p>First Page</p> -->
-        <?php
- 
-    require "load.php";
-    $ObjLayouts->heading();
-    $ObjMenus->main_menu();
-    $ObjHeadings->main_banner();
-    $ObjCont->about_content();
-    $ObjCont->side_bar();
-    $ObjLayouts->footer();
-    ?>
+<?php
 
-    </body>
-    </html>
-    
-   
+require "load.php";
+$ObjLayouts->heading();
+$ObjMenus->main_menu();
+$ObjHeadings->main_banner();
+$ObjCont->main_content();
+$ObjCont->side_bar();
+$ObjLayouts->footer();
+?>
